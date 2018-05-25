e9442d1a557d0ad71b9188365fb3cd1038cf3597
gandryeanb <gstandrianb@gmail.com>
2018-05-25 16:55:56 +0700

    adding new file

--- p0-w2-e7/p0-w2-e7_loop-asterik.js
+++ p0-w2-e7/p0-w2-e7_loop-asterik.js
@@ -1,18 +1,31 @@
 for (var a = 5; a > 0; a--) {
   console.log("*");
 }
-console.log("");
-var b = 5
 
-for (var a = b; b > 0; b--) {
-  console.log("*****")
-    for (var a = b; b > 3; b--) {
-      console.log("*****")
-    }
+for (var a = 5; a > 0; a--) {
+  var b = "";
+  for (var c = 5; c > 0; c--)  {
+    b = b +"*";
+  }
+  console.log(b)
 }
+
 console.log("");
+
+var c = 1;
+for (var a = 5; a > 0; a--) {
+  var b = "";
+  var d = 0;
+  for (var d = c; d > 0; d--) {
+    b = b + "*";
+  }
+  c = c + 1;
+  console.log(b);
+}
+
+/*console.log("");
 var c = "";
 for (var a = 0; a < 5; a++) {
   console.log(c = c+"*");
-}
+}*/
 
