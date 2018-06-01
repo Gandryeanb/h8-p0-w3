e2d0504bbc1bf653911b3c8abebe48ef4dd34816
gandryeanb <gstandrianb@gmail.com>
2018-06-01 23:36:52 +0700

    adding task e6

--- p0-w3-e6/p0-w3-e6.js
+++ p0-w3-e6/p0-w3-e6.js
@@ -1,9 +1,9 @@
 function angkaPalindrome(num) { 
   for ( var i = 0 ; i <= 10 ; i++) {
+    num++
     var numStr = String(num);  
     if (numStr[0] === numStr[numStr.length - 1]) {
       return num;
     }
-    num++  
   }
 }
